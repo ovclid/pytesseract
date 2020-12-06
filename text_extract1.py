97fae505870d29a1715c733827f2c7a2a14f0c84
sejong <ovclid01@gmail.com>
2020-12-06 11:07:39 +0900

    Update text_extract1.py

--- text_extract1.py
+++ text_extract1.py
@@ -1,9 +1,9 @@
 from PIL import Image
 import pytesseract
 
-value = Image.open("1.jpg")
-
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+value = Image.open("2.jpg")
 text = pytesseract.image_to_string(value, lang ="kor+eng")
 print(text)
+
